6325d4a9aa44d3acc8ef156c4d3844dedc3a1db9
Cheuk Yin Ng <chucksys88@gmail.com>
2017-10-19 20:28:30 -0700

    Initial commit

---
+++ assignment1/app.js
@@ -0,0 +1,26 @@
+(function() {
+    'use strict';
+
+    angular.module("LunchCheck", [])
+    .controller("LunchCheckController", LunchCheckController);
+
+    LunchCheckController.$inject = ["$scope"];
+    function LunchCheckController($scope) {
+        $scope.output = "";
+        $scope.list = "";
+        $scope.submit = function() {
+            // Count the number of items in the list
+            let count = 1;
+            for (let c of $scope.list) {
+                if (c == ',') ++count;
+            }
+
+            if ($scope.list == "")
+                $scope.output = "Please enter data first";
+            else if (count <= 3)
+                $scope.output = "Enjoy!";
+            else
+                $scope.output = "Too much!";
+        };
+    }
+})();
